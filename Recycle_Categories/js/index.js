6c0665a0d0b3c3990dda1773e3e10a7729a3e86f
sobaint01 <walebellomail@gmail.com>
2022-03-08 01:39:03 -0500

    Jquery Update

--- Recycle_Categories/js/index.js
+++ Recycle_Categories/js/index.js
@@ -3,7 +3,7 @@ function initMap() {
     // The location of atlanta
     const atlanta = { lat: 33.904, lng: -84.470 };
     // The map, centered at atlanta
-    const map = new google.maps.Map(document.getElementById("#map"), {
+    const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
       center: atlanta,
     });
