60c45f7036e32c4b427a9c78ac4d7e6d96bdc90b
sobaint01 <walebellomail@gmail.com>
2022-03-08 01:34:38 -0500

    Jquery Upadates

--- MyProfile/js/index.js
+++ MyProfile/js/index.js
@@ -4,7 +4,7 @@ function initMap() {
     // The location of atlanta
     const atlanta = { lat: 33.904, lng: -84.470 };
     // The map, centered at atlanta
-    const map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById("#map"), {
       zoom: 4,
       center: atlanta,
     });
